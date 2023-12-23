971f5ca25627a727dc3fed5f12918bd36c78074e
dreac0nic <matthewqa@gmail.com>
2023-12-22 16:13:12 -0800

    Write a quick and easy slurp function

--- cl-advent-of-code.lisp
+++ cl-advent-of-code.lisp
@@ -6,3 +6,10 @@
 (defvar *protocol* "https")
 (defvar *domain* "adventofcode.com")
 
+
+(defun slurp (path)
+  "Reads in entire file specified by path as a string"
+  (with-open-file (stream path)
+    (let ((buffer (make-string (file-length stream))))
+      (read-sequence buffer stream)
+      (string-trim '(#\Space #\Newline #\Tab) buffer))))
